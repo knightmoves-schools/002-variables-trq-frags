7d7fa766aa26119c4ed345508b26060215e1cded
track-frags <jadirks@anamosa.k12.ia.us>
2026-01-06 17:14:12 +0000

    im slow.... a PUBLIC CONST

--- Course.cs
+++ Course.cs
@@ -3,14 +3,12 @@ public class Course
 {   
     public int MaxStudents = 25;
 
-    public int PerfectScore = 100;
+    public const int PerfectScore = 100;
 
     
     public int CourseLength(){
         int courseLength;
         courseLength = 20;
-
-        PerfectScore = courseLength;
 
         return courseLength;
     }
