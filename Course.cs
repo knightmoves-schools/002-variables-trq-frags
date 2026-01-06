45928705be6c92fc585cd01e5703e93aabca267c
track-frags <jadirks@anamosa.k12.ia.us>
2026-01-06 17:10:49 +0000

    this?

--- Course.cs
+++ Course.cs
@@ -4,10 +4,13 @@ public class Course
     public int MaxStudents = 25;
 
     public int PerfectScore = 100;
+
     
     public int CourseLength(){
         int courseLength;
         courseLength = 20;
+
+        PerfectScore = courseLength;
 
         return courseLength;
     }
